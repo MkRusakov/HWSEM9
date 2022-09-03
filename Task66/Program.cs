37e42c357bce15a8d519ee400a026616928cd2bc
Mikhail <i@MkRusakov.ru>
2022-09-03 11:27:51 +0300

    task68

---
+++ Task66/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SumElements (int M, int N)
+{
+    int sum = M + (N - M);
+    if (M > N - 1) return sum;
+    SumElements(M + 1, N);
+    return sum;
+}
+int sum = SumElements(1, 5);
+Console.Write(sum);
+
+
---
+++ Task68/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int AckermannFunction (int M, int N)
+{
+    int res = 0;
+    if (M == 0)
+        return  res = N + 1;
+    else if ((M > 0) && (N == 0))
+        return res = AckermannFunction(M - 1, 1);
+    else return res = AckermannFunction(M - 1, AckermannFunction(M, N - 1));
+}
+int res = AckermannFunction(1, 2);
+Console.Write(res);
